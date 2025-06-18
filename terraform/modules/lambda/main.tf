a7ba9795a37e782c53cdd100aaafb2fde2fe5098
Milton Palmeira <lpsantana_@hotmail.com>
2025-06-17 22:40:14 -0300

    add: variables and main code in terraform modules commented

--- terraform/modules/lambda/main.tf
+++ terraform/modules/lambda/main.tf
@@ -1,16 +1,21 @@
+# Cria a função Lambda na AWS
 resource "aws_lambda_function" "this" {
   function_name = var.name
   handler = var.handler
   runtime = "nodejs18.x"
   role = var.role_arn
   filename = var.filename
+
+  # Garante que o deploy da função ocorra só se o hash do arquivo mudar
   source_code_hash = filebase64sha256(var.filename)
 
+  # Variáveis de ambiente da Lambda (como secrets, configs)
   environment {
     variables = var.env_vars
   }
 }
 
+# Cria um grupo de logs no CloudWatch para a Lambda
 resource "aws_log_group" "this" {
   name = "cw-lg-${aws_lambda_function.this.function_name}"
   retention_in_days = 14
--- terraform/modules/lambda/variables.tf
+++ terraform/modules/lambda/variables.tf
@@ -1,8 +1,30 @@
-variable "name" {}
-variable "handler" {}
-variable "role_arn" {}
-variable "filename" {}
+# Nome da função Lambda
+variable "name" {
+  description = "Nome da função Lambda"
+  type        = string
+}
+
+# Handler padrão da Lambda, por exemplo 'index.handler'
+variable "handler" {
+  description = "Handler da Lambda (arquivo.método)"
+  type        = string
+}
+
+# ARN da Role IAM que a Lambda vai assumir (permissões)
+variable "role_arn" {
+  description = "ARN da IAM Role que será associada à Lambda"
+  type        = string
+}
+
+# Caminho para o arquivo ZIP da função Lambda
+variable "filename" {
+  description = "Caminho para o arquivo ZIP com o código da Lambda"
+  type        = string
+}
+
+# Variáveis de ambiente que a Lambda vai usar
 variable "env_vars" {
+  description = "Variáveis de ambiente para a Lambda"
   type    = map(string)
   default = {}
 }
